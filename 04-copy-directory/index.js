8a2996b1b4088d00f0f534973b6a28869b0b4416
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2022-11-06 19:42:06 +0300

    feat: 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,53 @@
+const fs = require('fs'),
+  path = require('path');
 
+fs.readdir(__dirname, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+  else {
+    let filesCopyStatus = false;
+    files.forEach(file => {
+      if (!file.isFile() && file.name == 'files-copy') {
+        filesCopyStatus = true;
+        clearDir();
+        createAndCopyFiles();
+      }
+    });
+    if (!filesCopyStatus) {
+      fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+        if(err) throw err;
+      });
+      createAndCopyFiles();
+    }
+  }
+});
+
+function createFile (fileName) {
+  fs.writeFile (path.join(__dirname, 'files-copy', fileName), '', (err) => {
+    if(err) console.log(err);
+  });
+  return path.join(__dirname, 'files-copy', fileName);
+}
+
+function createAndCopyFiles() {
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file), createFile(file), (err) => {
+          if(err) console.log(err);
+        });
+      });
+    }
+  });
+}
+
+function clearDir() {
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    if (err) console.log(err);
+    for (const file of files) {
+      fs.unlink(path.join(path.join(__dirname, 'files-copy'), file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+}
